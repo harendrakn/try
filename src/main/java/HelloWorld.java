d8ab04028cf20a410bfb1e99d182c3ceb02c71a2
HK <hkumar.web@gmail.com>
2021-07-05 21:40:07 +0100

    refactored code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        writeMessages();
+    }
+
+    static void writeMessages(){
         System.out.println("Hello Everyone!!!");
         System.out.println("Another Line");
         System.out.println("This is my second line");
     }
+
 }
